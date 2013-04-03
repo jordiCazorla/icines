84ddd7de5150797fa7a823fdeb364d2a2150400c
jordiCazorla <romojojo@gmail.com>
2013-04-03 20:27:54 +0200

    [FEATURE] Add users in db.

--- nodecellar/server.js
+++ nodecellar/server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    wine = require('./routes/wines');
+    wine = require('./routes/wines'),
+    user = require('./routes/users');
 
 var app = express();
 
@@ -13,6 +14,12 @@ app.get('/wines/:id', wine.findById);
 app.post('/wines', wine.addWine);
 app.put('/wines/:id', wine.updateWine);
 app.delete('/wines/:id', wine.deleteWine);
+
+app.get('/users', user.findAll);
+app.get('/users/:id', user.findById);
+app.post('/users', user.addUser);
+app.put('/users/:id', user.updateUser);
+app.delete('/users/:id', user.deleteUser);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
