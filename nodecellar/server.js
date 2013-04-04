8e254262944ebbf2ae5406ba8b6c21140efd9302
jordiCazorla <romojojo@gmail.com>
2013-04-04 16:04:01 +0200

    [FIX] Take out wines.js

--- nodecellar/server.js
+++ nodecellar/server.js
@@ -1,5 +1,4 @@
 var express = require('express'),
-    wine = require('./routes/wines'),
     user = require('./routes/users');
 
 var app = express();
@@ -8,12 +7,6 @@ app.configure(function () {
     app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
 });
-
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
 
 app.get('/users', user.findAll);
 app.get('/users/:id', user.findById);
